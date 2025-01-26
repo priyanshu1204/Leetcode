c8d6d7a65b401f5a8945ba526abf1498ab5ba75d
Priyanshu Raj <115916385+priyanshu1204@users.noreply.github.com>
2025-01-27 00:51:58 +0530

    Time: 3 ms (83.45%), Space: 46.5 MB (5.01%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,24 +1,31 @@
 class Solution {
-    public int candy(int[] r) {
-        int n = r.length;
-        int[] c = new int[n];
-
-        for (int i = 0; i < n - 1; ++i) {
-            if(r[i] < r[i + 1])
-                if(c[i] >= c[i+1])
-                    c[i+1] = c[i] + 1;
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+        
+        // Initialize all children with 1 candy
+        Arrays.fill(candies, 1);
+        
+        // Left to right pass
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
         }
-
-        for (int i = n - 1; i > 0; --i) {
-            if(r[i] < r[i - 1])
-                if(c[i] >= c[i-1])
-                    c[i-1] = c[i] + 1;
-        }        
-
-        int ans = n;
-        for(int i = 0; i < n; ++i)
-            ans+= c[i];
-
-        return ans;
+        
+        // Right to left pass
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+        
+        // Sum up all candies
+        int totalCandies = 0;
+        for (int candy : candies) {
+            totalCandies += candy;
+        }
+        
+        return totalCandies;
     }
 }
