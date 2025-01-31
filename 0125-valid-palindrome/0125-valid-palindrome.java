73673ffd646906f10583bf0778ae0867f2883dab
Priyanshu Raj <115916385+priyanshu1204@users.noreply.github.com>
2025-01-31 19:56:17 +0530

    Time: 13 ms (42.61%), Space: 45.3 MB (20.83%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,32 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if(s == null){
-         return false;
-        }
-        if(s.length() == 0){ return false;
-        }
-        s = s.toLowerCase();
-        int i = 0;
-        int j = s.length() - 1;
-        while(i<j)
-        {
-            if(! ((s.charAt(i) >= 'a' && s.charAt(i) <= 'z' ) || (s.charAt(i) >= '0' && s.charAt(i)<='9'))){
-                i++;
-                continue;
-            }
-            if(! ((s.charAt(j) >= 'a' && s.charAt(j) <= 'z' ) || (s.charAt(j) >= '0' && s.charAt(j)<='9'))){
-                j--;
-                continue;}
-            if(s.charAt(i) == s.charAt(j)){
-                i++;
-                j--;
-                continue;}
-            if(s.charAt(i) != s.charAt(j))
-            {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
                 return false;
             }
+            left++;
+            right--;
         }
-        return true;
-        
+        return true;        
     }
 }
