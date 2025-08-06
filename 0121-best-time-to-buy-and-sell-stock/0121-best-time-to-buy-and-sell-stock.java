3e86f1dbcf29be9fc085c46a91db7f4155ef7380
Priyanshu Raj <115916385+priyanshu1204@users.noreply.github.com>
2025-08-06 15:54:13 +0530

    Time: 2 ms (71.93%), Space: 62.3 MB (7%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,21 +1,19 @@
-//Apna Collage
-//choose the day to buy min price stock
-//and from the next day compare and sell at day when the profit is minimum
 class Solution {
     public int maxProfit(int[] prices) {
-        int buyPrice = Integer.MAX_VALUE;
-        int profit = 0;
-        int dayProfit = 0;
-        
-        for(int i = 0; i < prices.length; i++){
-            if(prices[i] < buyPrice){
-                buyPrice = prices[i];
+        int l =0, r = 1;
+        int maxP = 0;
+
+        while(r < prices.length){
+            if(prices[l] < prices[r]){
+                int profit = prices[r] - prices[l];
+                maxP = Math.max(maxP , profit);
             }
-            dayProfit = prices[i] - buyPrice;
-            if(profit < dayProfit){
-                profit = dayProfit;
+            else{
+                l =r;
             }
+            r++;
         }
-        return profit;
+        return maxP;
+        
     }
 }
