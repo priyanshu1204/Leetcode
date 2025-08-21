57e645f4a514e145b9791152ffe2b01d22d7b876
Priyanshu Raj <115916385+priyanshu1204@users.noreply.github.com>
2025-08-21 14:39:09 +0530

    Time: 7 ms (21.02%), Space: 42.9 MB (43.73%) - LeetHub

---
+++ 3353-existence-of-a-substring-in-a-string-and-its-reverse/3353-existence-of-a-substring-in-a-string-and-its-reverse.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isSubstringPresent(String s) {
+        Set<String> seen = new HashSet<>();
+         for (int i = 0; i < s.length() - 1; i++) {
+            String sub = s.substring(i, i + 2);
+             // Case 1: same characters like "aa"
+            if (sub.charAt(0) == sub.charAt(1)) return true;
+             // Case 2: check if reverse substring is already seen
+            String rev = "" + sub.charAt(1) + sub.charAt(0);
+            if (seen.contains(rev)) return true;
+
+            // store the current pair
+            seen.add(sub);
+        }
+        return false;
+    }
+
+}
